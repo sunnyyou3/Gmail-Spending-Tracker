bc73b3ea745d5a8f7a797d2f663279ae6b6a2699
Sunny You <yousunny3@gmail.com>
2026-03-26 16:13:07 +0900

    Formatting

--- Gmail_Spending_Tracker/app/login.tsx
+++ Gmail_Spending_Tracker/app/login.tsx
@@ -1,12 +1,5 @@
 import React from "react";
-import {
-  View,
-  Text,
-  StyleSheet,
-  TextInput,
-  TouchableOpacity,
-} from "react-native";
-import { LinearGradient } from "expo-linear-gradient";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 
 const Login = () => {
   return (
