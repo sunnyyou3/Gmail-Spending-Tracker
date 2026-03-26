1460c34198e9140c1adb95d6d41a8a8aef7da8d3
Sunny You <yousunny3@gmail.com>
2026-03-26 15:55:04 +0900

    Updated landing page with app relevant text

--- Gmail_Spending_Tracker/app/landing.tsx
+++ Gmail_Spending_Tracker/app/landing.tsx
@@ -1,29 +1,51 @@
-import { View, StyleSheet } from "react-native";
-import { Text } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity  } from "react-native";
 import GradientText  from "@/components/GradientText";
 import { Link } from "expo-router";
 import PillText from "@/components/PillText";
 
 const Landing = () => {
   return (
-    <View style={landingStyles.container}>
-      <View style={landingStyles.pillContainer}>
-        <PillText>Lightning Fast</PillText>
-        <PillText>Secure by Default</PillText>
+    <View style={{flex: 1}}>
+      <View style={landingStyles.header}>
+        <View style={landingStyles.leftHeader}>
+          <GradientText style={{fontWeight: "bold", fontSize: 18}} gradientColors={["#a78bfa", "#22d3ee"]}>
+              Pulse
+          </GradientText>
+        </View>
+        <View style={landingStyles.nav}>
+        <TouchableOpacity>
+          <Text style={landingStyles.link}>Features</Text>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Text style={landingStyles.link}>Guide</Text>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Text style={landingStyles.link}>About</Text>
+        </TouchableOpacity>
       </View>
-      <Text style={landingStyles.headline}>Ship faster with{' '}
-        <GradientText style={landingStyles.headline} gradientColors={["#a78bfa", "#22d3ee"]}>
-          Pulse
-        </GradientText>
-      </Text>
-      <Text style={landingStyles.description}>
-        The modern developer platform that turns ideas into production-ready apps — no boilerplate, no hassle.
-      </Text>
-      <Link href="./login">
-        <GradientText style={landingStyles.buttonText} gradientColors={[ "#7c3aed", "#6d28d9", "#7c3aed", "#8b5cf6"]}>
-          Get Started
-        </GradientText>
-      </Link>
+      </View>
+      <View style={landingStyles.container}>
+          <View style={landingStyles.pillContainer}>
+            <PillText>Lightning Fast</PillText>
+            <PillText>Secure by Default</PillText>
+          </View>
+          <Text style={landingStyles.headline}>Budget Better With{' '}
+            <GradientText style={landingStyles.headline} gradientColors={["#a78bfa", "#22d3ee"]}>
+              Pulse
+            </GradientText>
+          </Text>
+          <Text style={landingStyles.description}>
+            The smart finance app that turns your emails into real-time spending insights — no manual tracking, no hassle.
+          </Text>
+          <Link href="./login">
+            <GradientText style={landingStyles.buttonText} gradientColors={[ "#7c3aed", "#6d28d9", "#7c3aed", "#8b5cf6"]}>
+              Get Started
+            </GradientText>
+          </Link>
+          <Text style={landingStyles.bottomText}>
+            Your inbox knows your spending—we make it visible.
+          </Text>
+        </View>
     </View>
   );
 }
@@ -39,11 +61,24 @@ const landingStyles = StyleSheet.create({
     justifyContent: 'center',
     paddingHorizontal: 24
   },
+  header: {
+    zIndex: 10,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingHorizontal: 70,
+    paddingTop: 30,
+  },
+  leftHeader: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 8,
+  },
   pillContainer: {
     flexDirection: 'row',
     flexWrap: 'wrap',
     justifyContent: 'center',
-    marginBottom: 32,
+    marginBottom: 40,
     gap: 12
   },
   headline: {
@@ -59,17 +94,37 @@ const landingStyles = StyleSheet.create({
     position: "relative",
     fontFamily: "DM Sans, sans-serif",
     fontSize: 17.6,
+    textAlign: "center",
     color: "#8a8a9a",
     marginTop: 40,
     marginBottom: 20,
-    maxWidth: 412,
+    maxWidth: 450,
     lineHeight: 30
   },
   buttonText: {
     fontFamily: "Sora, sans-serif",
     color: "#0a0a0f",
     position: "relative"
-  }
+  },
+  nav: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 24,
+  },
+  link: {
+    fontSize: 18,
+    color: "#8a8a9a",
+    fontWeight: "bold",
+    fontFamily: "DM Sans",
+  },
+  bottomText: {
+    marginTop: 48,
+    fontSize: 12,
+    letterSpacing: 1.5,
+    textTransform: "uppercase",
+    color: "#4a4a5a",
+    // fontFamily: "DM Sans",
+  },
 });
 
 export default Landing;
